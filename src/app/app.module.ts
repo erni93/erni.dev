db7136571f269e7a45d973c8bb24e1a6cffae8f3
Ernesto del valle <erni.delvalle@outlook.com>
2020-02-14 13:26:42 +0100

    add markdown component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { ProfilePhotoComponent } from "../components/profile-photo/profile-photo.component";
 import { SectionTitleComponent } from "../components/shared/section-title/section-title.component";
+import { MarkdownComponent } from "../components/shared/markdown/markdown.component";
 
 @NgModule({
-  declarations: [AppComponent, ProfilePhotoComponent, SectionTitleComponent],
+  declarations: [
+    AppComponent,
+    ProfilePhotoComponent,
+    SectionTitleComponent,
+    MarkdownComponent
+  ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/components/shared/markdown/markdown.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from "@angular/core";
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
+import marked from "marked";
+import DOMPurify from "dompurify";
+
+@Component({
+  selector: "app-markdown",
+  template: '<div [innerHTML]="data"></div>'
+})
+export class MarkdownComponent {
+  @Input("text")
+  set text(value: string) {
+    this.data = this.markdownToSafeHtml(value);
+  }
+
+  data: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  markdownToSafeHtml(value: string): SafeHtml {
+    const html = marked(value);
+    const safeHtml = DOMPurify.sanitize(html);
+    return this.sanitizer.bypassSecurityTrustHtml(safeHtml);
+  }
+}
